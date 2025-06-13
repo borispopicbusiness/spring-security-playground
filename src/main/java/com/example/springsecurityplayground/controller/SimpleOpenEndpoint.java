87396a8ef288e48a664982ab1a42e12f2b2a08a4
Boris Popic <borispopic.business@gmail.com>
2025-06-13 17:33:15 +0200

    wip: create SimpleOpenEndpoint interface and SimpleOpenEndpointImpl

---
+++ src/main/java/com/example/springsecurityplayground/controller/SimpleOpenEndpoint.java
@@ -0,0 +1,5 @@
+package com.example.springsecurityplayground.controller;
+
+public interface SimpleOpenEndpoint {
+    
+}
---
+++ src/main/java/com/example/springsecurityplayground/controller/impl/SimpleOpenEndpointImpl.java
@@ -0,0 +1,6 @@
+package com.example.springsecurityplayground.controller.impl;
+
+import com.example.springsecurityplayground.controller.SimpleOpenEndpoint;
+
+public class SimpleOpenEndpointImpl implements SimpleOpenEndpoint {
+}
